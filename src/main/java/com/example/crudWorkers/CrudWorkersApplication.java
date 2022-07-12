f6ef1c91f3516740b3358f26d66a6cdba821ae2a
Mariano Chun <marianochun01@gmail.com>
2022-07-12 19:15:45 -0300

    Initial commit

---
+++ src/main/java/com/example/crudWorkers/CrudWorkersApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudWorkers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudWorkersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudWorkersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crudWorkers/CrudWorkersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudWorkers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudWorkersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
